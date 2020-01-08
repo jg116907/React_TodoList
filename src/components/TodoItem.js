2659443007677f2eaf6af90e915c54e1613ad22e
jg116907 <jg116907@naver.com>
2020-01-09 01:46:15 +0900

    init

---
+++ src/components/TodoItem.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import './TodoItem.css';
+
+class TodoItem extends Component {
+  render(){
+    const {text,checked,id,onToggle,onRemove} = this.props;
+    return (
+      <div className="todo-item" onClick={()=>onToggle(id)}>
+        <div className="remove" onClick={(e)=>{
+          e.stopPropagation(); //onToggle이 실행되지 않도록 한다. 이벤트의 확산을 멈춤
+          onRemove(id)}
+        }>&times;</div>
+        <div className={`todo-text ${checked && 'checked'}`}>
+          <div>{text}</div>
+        </div>
+        {
+          checked && (<div className="check-mark">✓</div>)
+        }
+      </div>
+    )
+  }
+}
+
+export default TodoItem;
---
+++ src/components/TodoItemList.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import TodoItem from './TodoItem';
+
+class TodoItemList extends Component{
+  render() {
+    const {todos, onToggle, onRemove } = this.props;
+    // todos : todo 객체들이 들어있는 배열
+    // onToggle : 체크박스를 키고 끄는 함수
+    // onRemove : 아이템을 삭제시키는 함수
+    return (
+      <div>
+        <TodoItem text="안녕하세요."/>
+        <TodoItem text="반갑습니다."/>
+        <TodoItem text="안녕히계세요."/>
+      </div>
+    );
+  }
+}
+
+export default TodoItemList;
