2203bc7d088fd6ab18aba05c6c309fc9afdb9342
jg116907 <jg116907@naver.com>
2020-01-09 15:24:29 +0900

    ENHANCE component optimization

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,8 +2,13 @@ import React,{Component} from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component {
+  // ------ 컴포넌트 최적화
+  shouldComponentUpdate(nextProps, nextState){
+    return this.props.checked !== nextProps.checked;
+  }
   render(){
-    const {text,checked,id,onToggle,onRemove} = this.props;
+    const { text, checked, id, onToggle, onRemove } = this.props;
+    console.log(id); // component 효율 체크
     return (
       <div className="todo-item" onClick={()=>onToggle(id)}>
         <div className="remove" onClick={(e)=>{
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -2,6 +2,10 @@ import React,{Component} from 'react';
 import TodoItem from './TodoItem';
 
 class TodoItemList extends Component{
+  // ------ 컴포넌트 최적화 함수
+  shouldComponentUpdate(nextProps, nextState) { // 컴포넌트 라이프 사이클 메소드중 컴포넌트가 리렌더링을 할 지 말지 정해줌
+    return this.props.todos !== nextProps.todos; // todos 값이 바뀔 때만 리렌더링
+  }
   render() {
     const {todos, onToggle, onRemove } = this.props;
     // todos : todo 객체들이 들어있는 배열
